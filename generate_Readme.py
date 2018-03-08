3723b7e3439334412ae83304437dc859392e9e57
David Roehrscheid <drwebservice@gmail.com>
2018-03-08 18:44:50 +0900

    Add comments to py file.

--- generate_Readme.py
+++ generate_Readme.py
@@ -7,6 +7,11 @@ from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 #%matplotlib inline
 
+
+# ----------------------------------------------------
+# Output Readme.md markdown file that list out my books_list
+# ----------------------------------------------------
+
 now = datetime.datetime.now()
 
 print("# Literature log")
@@ -15,10 +20,12 @@ print("I started this log on October 30th, 2012. I list all books I read.")
 print("")
 
 total_books = 0
+# open and parse XML database
 e = xml.etree.ElementTree.parse('log.xml').getroot()
 books = e.findall('book')
 total_books = len(books)
 
+# Print general statistics
 print("### Statistics")
 books_per_month = (total_books/( (now.year - 2013.0)*12.0 + 4.0 + now.month ))
 print("Total number of books read: " + str(total_books) + "  ")
@@ -29,6 +36,7 @@ print("![Books read per month](book_read.png)")
 print('')
 print("### List of books")
 
+# List out all books with author and date read
 books_list = []
 for atype in books:
     total_books += 1
@@ -39,16 +47,23 @@ for atype in books:
         print("Finished: " + atype.find('finnished').text)
     print("")
 
+# ---------------------------------------
 # Update graphs
+# ---------------------------------------
+
 df = pd.DataFrame(books_list, columns=['title', 'author', 'date_read'])
 df.date_read = pd.to_datetime(df.date_read, format="%Y.%m.%d")
 
+# We want to summarize by date - make the date the index
 df.index = df.date_read
+# summarize by year
 df2 = df.groupby(pd.Grouper(freq='A')).count()['title']
 
 df2.year = pd.Series(df2.index).apply(lambda x: x.year)
 df2.index = df2.year
 
+# plot total books recorded each year
+# Note: 2012 doesn't have data for the whole year, so it's books recorded, not book read
 my_dpi=100
 plt.figure(figsize=(580/my_dpi, 360/my_dpi), dpi=my_dpi)
 fig = df2.plot(kind='bar')
@@ -57,16 +72,17 @@ fig.set_ylabel('Number of books recorded')
 fig.set_title('Books recorded')
 fig.get_figure().savefig('book_recorded.png')
 
+# summarize by month
 df3 = df.groupby(pd.Grouper(freq='M')).count()['title']
 
-# extend to current month to also include recent
-# periods without activity
+# extend to current month to also include recent periods without activity
 start = max(df3.index)
 end = pd.Timestamp.today() + pd.tseries.offsets.MonthEnd()
 
-
+# update index including potentially empty months
 df3a = df3.reindex(df3.index.union(pd.date_range(start=start, end=end,freq="M"))).fillna(0)
 
+# plot
 plt.figure(figsize=(580/my_dpi, 360/my_dpi), dpi=my_dpi)
 df3df = pd.rolling_mean(df3a, 5, center=True)
 fig = df3df.plot(yticks=[0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
